449e3d767cf87b0573b55d9799baee9e6377b9ac
toemah <toemah@protonmail.com>
2021-02-13 19:08:01 +0100

    v1.0 - revamp completed

--- script/main.js
+++ script/main.js
@@ -1,5 +1,7 @@
 let icon = null,
 	toggle = null,
+	slides = null,
+	slidesSrc = null,
 	palette = {
 		bodyBgdColor: {
 			dark: '#151515',
@@ -12,6 +14,10 @@ let icon = null,
 		mainBrdColor: {
 			dark: '#111111',
 			light: '#d3d3d3'
+		},
+		sliderBtnBgdColor: {
+			dark: '#1e1e1e',
+			light: '#f1f1f1'
 		},
 		logoSrc: {
 			dark: 'media/github_dark.png',
@@ -36,6 +42,9 @@ function switchTheme() {
 		mainBrdColor = rgb2hex(window.getComputedStyle(main).borderBottomColor);
 	main.style.backgroundColor = mainBgdColor != palette.mainBgdColor.light ? palette.mainBgdColor.light : palette.mainBgdColor.dark;
 	main.style.borderColor = mainBrdColor != palette.mainBrdColor.light ? palette.mainBrdColor.light : palette.mainBrdColor.dark;
+	Array.from(document.getElementsByClassName('slider-button')).forEach(e => {
+		e.style.backgroundColor = rgb2hex(window.getComputedStyle(e).backgroundColor) != palette.sliderBtnBgdColor.light ? palette.sliderBtnBgdColor.light : palette.sliderBtnBgdColor.dark;
+	})
 	let logo = document.getElementById('footer').children[0],
 		logoSrc = logo.getAttribute('src');
 	logo.setAttribute('src', logoSrc != palette.logoSrc.dark ? palette.logoSrc.dark : palette.logoSrc.light);
@@ -74,8 +83,7 @@ function owofy(str) {
 function uwuModeActivated() {
 	icon.setAttribute('src', 'media/artwork_by_ninjer.webp');
 	icon.setAttribute('title', 'made by ninjer');
-	icon.removeEventListener('click', fullView);
-	icon.addEventListener('click', () => { window.open('https://twitter.com/_ninjer') });
+	icon.parentElement.href = "https://www.twitter.com/_ninjer"
 	document.querySelector('link').setAttribute('href', 'media/faviconya.png')
 	let p = document.getElementById('about').querySelector('p'),
 		t = document.title;
@@ -88,18 +96,49 @@ function fullView() {
 	let src = this.getAttribute('src'),
 		img = document.createElement('img'),
 		body = document.body;
-	img.setAttribute('src', `${src.substr(0, src.indexOf('.'))}.png`);
+	if (src.split('.')[1] != "png") {
+		img.setAttribute('src', `${src.substr(0, src.indexOf('.'))}.png`);
+	} else {
+		img.setAttribute('src', src);
+	}
 	img.setAttribute('style', 'height: 77%; box-shadow: 0 0 31px 12px black; position: absolute; z-index: 1;');
 	img.setAttribute('title', 'click me to close!');
 	img.addEventListener('click', () => body.removeChild(body.children.item(body.children.length - 1)));
 	body.appendChild(img);
 }
 
+function prevSlide() {
+	updateSlideshow(-1);
+}
+
+function nextSlide() {
+	updateSlideshow(1);
+}
+
+function updateSlideshow(n) {
+	slides.forEach(e => {
+		let i = slidesSrc.indexOf(e.getAttribute('src'))+n,
+			index = i >= slidesSrc.length ? 0 : i < 0 ? slidesSrc.length-1 : i;
+		e.setAttribute('src', slidesSrc[index]);
+	});
+}
+
 window.addEventListener('load', () => {
 	icon = document.getElementById('icon');
-	icon.addEventListener('click', fullView);
 	toggle = document.getElementById('toggle');
 	toggle.addEventListener('click', switchTheme);
+	slides = Array.from(document.getElementById('slideshow').children);
+	slidesSrc = [];
+	slides.forEach(e => {
+		if (slides.indexOf(e) != Math.floor(slides.length / 2)) {
+			e.style.filter = `blur(${Math.abs(slides.indexOf(e) - (slides.length) / (slides.length / 2))}px)`;
+		} else {
+			e.addEventListener('click', fullView);
+		}
+		slidesSrc.push(e.getAttribute('src'));
+	});
+	document.getElementById('prev').addEventListener('click', prevSlide);
+	document.getElementById('next').addEventListener('click', nextSlide);
 	if (Math.random().toFixed(2) == 0.69) {
 		uwuModeActivated();
 	} else {

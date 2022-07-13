76863b20410a2f25355b0737f26d21777a536a3f
thomas <toemah@protonmail.com>
2022-07-13 17:02:21 +0200

    Update main.js
    adds switchTheme function

--- script/main.js
+++ script/main.js
@@ -1,5 +1,22 @@
 const birthday = "2001-12-06";
 const age = document.querySelector("#age");
+const head = document.querySelector("#head");
+const section = document.querySelectorAll(".section");
+
+const theme = {
+    light: {
+        body: "body-light",
+        head: "head-light",
+        section: "section-light"
+    },
+    dark: {
+        body: "body-dark",
+        head: "head-dark",
+        section: "section-dark"
+    }
+}
+
+let selectedTheme = theme.light;
 
 const calculateYears = function (date) {
     let now = new Date();
@@ -15,3 +32,17 @@ const calculateYears = function (date) {
 window.addEventListener("load", () => {
     age.innerText = `${calculateYears(birthday)}yo`;
 });
+
+const switchTheme = function() {
+    if(selectedTheme == theme.light) {
+        selectedTheme = theme.dark;
+        document.body.classList.replace(theme.light.body, selectedTheme.body);
+        head.classList.replace(theme.light.head, selectedTheme.head);
+        section.forEach(e => e.classList.replace(theme.light.section, selectedTheme.section));
+    } else {
+        selectedTheme = theme.light;
+        document.body.classList.replace(theme.dark.body, selectedTheme.body);
+        head.classList.replace(theme.dark.head, selectedTheme.head);
+        section.forEach(e => e.classList.replace(theme.dark.section, selectedTheme.section));
+    }
+}
